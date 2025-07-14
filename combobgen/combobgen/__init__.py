4bdffcf17d502c4c65f3776f87b3a5167a2e984b
MakimaGorky <maidvital@gmail.com>
2025-07-14 14:23:48 +0300

    lol

--- combobgen/combobgen/__init__.py
+++ combobgen/combobgen/__init__.py
@@ -1,17 +1,14 @@
-import random
-import math
-from collections import defaultdict, Counter
-from typing import List, Any, Iterator, Callable
-import time
 
-import cryptography
+
+from .functions import *
 
 __version__ = '0.0.0.0'
 
-__all__ = [
-    'random', 
-    'math', 
-    'defaultdict', 'Counter', 
-    'List', 'Any', 'Iterator', 'Callable', 
-    'time'
-]
+# __all__ = [
+#     'random', 
+#     'math', 
+#     'defaultdict', 'Counter', 
+#     'List', 'Any', 'Iterator', 'Callable', 
+#     'time',
+#     'cryptography',
+# ]
